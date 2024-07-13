2d0ebb79097e48c4e20f8038d03cd32330ab8eb0
SvetiVos <svetavostokova766@gmail.com>
2024-07-13 16:29:21 +0300

    Добавила категории

---
+++ src/main/java/com/example/repositories/NoteRepository.java
@@ -0,0 +1,17 @@
+package com.example.repositories;
+
+import com.example.models.Note;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Sort;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findByTitle(String title);
+
+    List<Note> findByDateTime(String dateTime, Sort sort);
+
+    List<Note> findByCategory(String category, Sort sort);
+
+
+
+}
